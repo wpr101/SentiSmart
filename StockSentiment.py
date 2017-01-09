4e3d07b6e6c24279fd5f8fcfabc008e93fa0ddbe
wpr101 <wpr101@hotmail.com>
2017-01-09 13:37:35 +0200

    destructor removed

--- StockSentiment.py
+++ StockSentiment.py
@@ -3,7 +3,7 @@ from datetime import date
 #Idea is to calculate the percentage rise from earnings
 #Facebook reports on Wednesday February 1 after the close.
 #Price change will be Thursday opening price minus
-#Wednesday closing price
+#Wednesday closing 
 
 #symbol -> earnings date
 stocks = {'FB':[date(2017, 2, 1)],
