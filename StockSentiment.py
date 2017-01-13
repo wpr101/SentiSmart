d60ec64522ecf789216943e73fa5a0caea71f6fb
wpr101 <wpr101@hotmail.com>
2017-01-13 18:14:10 +0200

    ibd with earnings

--- StockSentiment.py
+++ StockSentiment.py
@@ -1,4 +1,5 @@
 from datetime import date
+import pandas as pd
 
 #Idea is to calculate the percentage rise from earnings
 #Facebook reports on Wednesday February 1 after the close.
@@ -11,4 +12,24 @@ stocks = {'FB':[date(2017, 2, 1)],
             'NOW':[date(2017, 1, 25)]
 }
 
+past_stocks = {'FB':[(date(2016, 11, 2), 5.7), (date(2016, 7, 27), 4.5), 
+                    (date(2016, 4, 27), 3.4), (date(2016, 1, 27), 4.8)],
+            'SHOP':[date(2017, 2, 15)],
+            'NOW':[date(2017, 1, 25)]
+}
+
+
+
+xl = pd.ExcelFile('IBD.xls')
+print(xl.sheet_names)
+
+df = xl.parse('Stock list', skiprows=8)
+my_stocks = []
+for index, row in df.iterrows():
+    my_stocks.append( (row['Symbol'], row['Earnings Date']))
+
+#sorted by earnings date
+for i in sorted(my_stocks, key=lambda earnings: earnings[1]):
+    print (i)
+    
 
