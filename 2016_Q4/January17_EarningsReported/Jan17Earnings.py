2f350756d3e1f3b6a52cdc603e12d135b84338d3
wpr101 <wpr101@hotmail.com>
2017-02-12 13:33:27 +0200

    earnings data

---
+++ 2016_Q4/January17_EarningsReported/Jan17Earnings.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.plotly as py
+from ggplot import *
+from matplotlib import *
+
+
+with open('Jan17EarningsData.txt') as f:
+    content = f.readlines()
+
+content = [x.strip() for x in content] 
+
+grouped_stocks = []
+for line in content:
+    symbol, release, EstEPS, ActualEPS = tuple(line.split(' '))
+    grouped_stocks.append(( symbol, release, EstEPS, ActualEPS))
+
+df = pd.DataFrame(grouped_stocks, columns=['Symbol', 'Release', 'EstEPS', 'ActualEPS'])
+symbol_list = df['Symbol'].tolist()
+
+df1 = df[['EstEPS', 'ActualEPS']]
+df1 = df1.astype(float)
+ax = df1[['EstEPS','ActualEPS']].plot(kind='bar', title ="Jan17 Earnings", figsize=(15, 10), legend=True, fontsize=12)
+ax.set_xlabel("Companies", fontsize=12)
+ax.set_ylabel("Earnings Per Share", fontsize=12)
+ax.set_xticklabels(symbol_list)
+plt.show()
