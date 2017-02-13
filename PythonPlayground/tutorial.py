ac1b42be7d28af1a1f245a1fd2a8b183de48e982
wpr101 <wpr101@hotmail.com>
2017-02-14 00:32:04 +0200

    plotting graphs

---
+++ PythonPlayground/tutorial.py
@@ -0,0 +1,31 @@
+import urllib2
+import time
+import datetime
+
+stocksToPull = 'AAPL','GOOGL','MSFT','CMG','AMZN','EBAY','TSLA'
+
+def pullData(stock):
+    try:
+        fileLine = stock+'.txt'
+        urlToVisit = 'http://chartapi.finance.yahoo.com/instrument/1.0/'+stock+'/chartdata;type=quote;range=1y/csv'
+        sourceCode = urllib2.urlopen(urlToVisit).read()
+        splitSource = sourceCode.split('\n')
+
+        for eachLine in splitSource:
+            splitLine = eachLine.split(',')
+            if len(splitLine) == 6:
+                if 'values' not in eachLine:
+                    saveFile = open(fileLine, 'a')
+                    lineToWrite = eachLine + '\n'
+                    saveFile.write(lineToWrite)
+                    
+        print 'Pulled', stock
+        print 'sleeping'
+        time.sleep(1)
+            
+    
+    except Exception, e:
+        print 'main loop', str(e)
+
+for eachStock in stocksToPull:
+    pullData(eachStock)
