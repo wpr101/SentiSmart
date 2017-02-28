4d6ab4d3d5fa9d7393ef2a5b4d9194de087a9508
wpr101 <wpr101@hotmail.com>
2017-03-01 00:54:31 +0200

    average random

--- SchwabScreens/CalculateReturns.py
+++ SchwabScreens/CalculateReturns.py
@@ -1,5 +1,9 @@
+from random import randint
+
 def percent_change(startPoint, currentPoint):
     return ((float(currentPoint) - startPoint)/abs(startPoint))*100.00
+
+MONTE_CARLO_SAMPLE_SIZE = 10000
 
 
 with open("Feb27_28/Results_Strat1.txt", "r") as f:
@@ -15,6 +19,11 @@ with open("Feb27_28/Results_Strat1.txt", "r") as f:
 
     #print(symbols_list)
     #print(returns_list)
+    '''returns_list = [5, percent_change(17.18,16.49), percent_change(830.68,808.57),
+                    percent_change(69.24,75.59), percent_change(40.72,38.53),
+                    percent_change(26.17,25.10), percent_change(63.52,55.12),
+                    percent_change(89.77,88.49), percent_change(84.84,88.00),
+                    percent_change(120.52,115.33), percent_change(21.81,22.79)]'''
 
     sum_returns = 0
     for i in range(len(returns_list)):
@@ -22,6 +31,34 @@ with open("Feb27_28/Results_Strat1.txt", "r") as f:
         #print('sum',sum_returns)
         #print('current',returns_list[i])
         #print('')
-    average_return = sum_returns/len(returns_list)   
-    print(average_return)
+    actual_return = sum_returns/len(returns_list)   
+    print('actual_return', round(actual_return,2))
+
+    
+    max_return = 0
+    min_return = 0
+    for a in range(0,MONTE_CARLO_SAMPLE_SIZE):
+        average_monte_carlo_return = 0
+        monte_carlo_return = 0
+        for i in range(len(returns_list)):
+            index = randint(0,len(returns_list)-1)
+            monte_carlo_return += returns_list[index]
+        average_monte_carlo_return = monte_carlo_return/len(returns_list)
+        #print('monte_carlo_return', round(average_monte_carlo_return,2))
+
+        if (average_monte_carlo_return > max_return):
+            max_return = average_monte_carlo_return
+
+        if (average_monte_carlo_return < min_return):
+            min_return = average_monte_carlo_return
+
+    print('max_random_return', round(max_return,2))
+    print('min_random_return', round(min_return,2))
+
+    averaged_random_return = 0
+    for i in range(MONTE_CARLO_SAMPLE_SIZE):
+        index = randint(0,len(returns_list)-1)
+        averaged_random_return += returns_list[index]
+    averaged_random_return = averaged_random_return/MONTE_CARLO_SAMPLE_SIZE
+    print('averaged_random_returns',round(averaged_random_return,2))
 
