06465587199ddf4e93d1a7005d2ebdea84d3be13
wpr101 <wpr101@hotmail.com>
2017-03-16 20:41:56 +0200

    trades for march 16

--- SchwabScreens/CalculateReturns.py
+++ SchwabScreens/CalculateReturns.py
@@ -8,7 +8,7 @@ import matplotlib
 matplotlib.style.use('ggplot')
 
 MONTE_CARLO_SAMPLE_SIZE = 10000
-path = 'Mar15_16/Everything_PE/'
+path = 'Mar15_16/BasicPE_Range_Technicals/'
 results_data = {}
 
 def percent_change(startPoint, currentPoint):
