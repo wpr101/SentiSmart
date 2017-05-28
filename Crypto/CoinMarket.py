2ebdc6689cc0f2219169a81fde58d88f396742f1
wpr101 <wpr101@hotmail.com>
2017-05-28 10:43:20 -0400

    writing to files

--- Crypto/CoinMarket.py
+++ Crypto/CoinMarket.py
@@ -1,50 +1,58 @@
 import json
+import os
+from datetime import datetime
 from coinmarketcap import Market
+from apscheduler.schedulers.blocking import BlockingScheduler
+
 coin_market = Market()
 json_data = coin_market.ticker().decode('utf-8')
 data = json.loads(json_data)
 
-for coin in data:
-    if coin['percent_change_1h'] is not None:
-        #check for volume over $1 million dollars
-        if float(coin['24h_volume_usd']) > 1000000:
-            percent_change_1h = float(coin['percent_change_1h'])
-            if percent_change_1h > 10:
-                print (coin['name'])
+path = 'Data'
+poloniex_symbols = ['ETH', 'XRP', 'ETC', 'GNT', 'STR', 'DOGE', 'XEM', 'LTC',
+                    'DGB', 'XMR', 'ZEC', 'SC', 'BCN', 'DASH', 'BTS', 'BTM',
+                    'FCT', 'STEEM', 'STRAT', 'REP', 'LSK', 'SYS', 'NXT',
+                    'MAID', 'GAME', 'ARDR', 'RIC', 'GNO', 'DCR', 'AMP', 'VTC',
+                    'CLAM', 'BURST', 'BCY', 'LBC', 'SJCX', 'NAV', 'PINK', 
+                    'BTCD', 'EXP', 'PPC', 'XCP', 'NEOS', 'NAUT', 'POT',
+                    'PASC', 'BELA', 'EMC2', 'BLK', 'FLO', 'FLDC', 'NMC', 'GRC',
+                    'XVC', 'XPM', 'OMNI', 'SBD', 'XBC', 'NXC', 'VRC', 'HUC',
+                    'NOTE'
+                    ]
+
+def increasing():
+    for coin in data:
+        if coin['percent_change_1h'] is not None:
+            #check for volume over $1 million dollars
+            if float(coin['24h_volume_usd']) > 1000000:
+                percent_change_1h = float(coin['percent_change_1h'])
+                if percent_change_1h > 10:
+                    print (coin['name'])
 
 
 
+def data_to_file():
+    directory = path + '/'
+            
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    text_file = open(directory + 
+                str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')) + "_results.txt", "w")
+    text_file.write('Coins meeting the filter' + '\n\n')
+    for coin in data:
+        change_1h = coin['percent_change_1h']
+        rank = coin['rank']
+        if (change_1h is None) or (rank is None):
+            continue
+        elif (float(change_1h) > 5) and (int(rank) <= 100):
+            text_file.write(str(coin['name']) + '\n')
+            text_file.write('Change_1h: ' + str(coin['percent_change_1h']) + '\n')
+            text_file.write('Rank ' + str(coin['rank']))
+            text_file.write('\n\n')
+
+    text_file.close()
 
 
-#HACK
-#returns a dictionary with data for that coin
-'''def get_coin_data(coin):
-    coin_data = {}
-
-    i = 0
-    items = []
-    while i < len(coin):
-        if coin[i] == '\"':
-            key = ''
-            i += 1
-            while(coin[i] != '\"'):
-                key += coin[i]
-                i += 1
-            items.append(key)
-        i += 1
-        
-    j = 0
-    while j < len(items):
-        coin_data[items[j]] = items[j+1]
-        j += 2
-
-    return(coin_data)
-
-coins = ['bitcoin', 'ethereum', 'litecoin', 'monero']
-for i in coins:
-    coin = coin_market.ticker(i)
-    coin = coin.decode("utf-8")
-    coin_data = get_coin_data(coin)
-    for key, value in coin_data.items():
-        print (key, value)
-    print('') '''
+scheduler = BlockingScheduler()
+scheduler.add_job(data_to_file, 'interval', minutes=.5)
+scheduler.start()
